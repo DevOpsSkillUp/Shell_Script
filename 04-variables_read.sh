45bc413a71577e2646e6bc935236a2277b3e883c
“medurubharath” <medurubharathstvd@gmail.com>
2024-07-31 17:58:08 -0500

    arrays file

--- 04-variables_read.sh
+++ 04-variables_read.sh
@@ -2,11 +2,7 @@
 
 echo "Hello Good Morning My dear Friend!"
 echo "Please enter your name::"
-
 read USERNAME
-
 echo "Please enter the password::"
-
 read -s PASSWORD
-
 echo "Username is: $USERNAME and the password is : $PASSWORD"
