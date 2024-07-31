0d69e64893da141f826a98f9e834548c452d0f8b
“medurubharath” <medurubharathstvd@gmail.com>
2024-07-31 12:59:06 -0500

    Added file

---
+++ 3-variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+Person1=$1
+Person2=$2
+
+#referring variable using parameters
+echo "$Person1: Hello How are your $Person2"
+echo "$Person2: I am good. What about you my friend?"
+
+
