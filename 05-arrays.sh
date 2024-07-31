4e3bb34d86d890d2824c6fd726abaac455c57b4d
“medurubharath” <medurubharathstvd@gmail.com>
2024-07-31 18:42:47 -0500

    files

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-arr=("Bharath" "Lokesh" "Honey")
+arr1=("Bharath" "Lokesh" "Honey")
 
-echo "I am ${arr[0]}"
-echo "I am ${arr[1]}"
-echo "I am ${arr[2]}"
+echo "I am ${arr1[0]}"
+echo "I am ${arr1[1]}"
+echo "I am ${arr1[2]}"
 
 
 #!/bin/bash
