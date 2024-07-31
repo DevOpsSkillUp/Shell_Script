34e8698d1aea0a7d875c300861de55609c27d428
“medurubharath” <medurubharathstvd@gmail.com>
2024-07-31 18:54:27 -0500

    files

--- 05-array_list.sh
+++ 05-array_list.sh
@@ -2,11 +2,11 @@
 
 MOVIES=("RRR" "DjTillu" "murari")
 
-# size of above array is 3.
-# index are 0,1,2
-
-# list always starts with 0.
 
 echo "First Movie is: ${MOVIES[0]}"
 echo "First Movie is: ${MOVIES[1]}"
 echo "First Movie is: ${MOVIES[2]}"
+
+arra=("Hello" "Good Morning" "Bye!")
+echo "${arra[0]} sir, i am ${arra[1]}"
+echo "${arra[2]}"
