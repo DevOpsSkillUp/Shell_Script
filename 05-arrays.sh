d4f1ee430d6ebd08c78a4a34e2211019a8c97bdb
“medurubharath” <medurubharathstvd@gmail.com>
2024-07-31 18:47:42 -0500

    files

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-arr1=("Bharath" "Lokesh" "Honey")
+MOVIES=("RRR" "DjTillu" "murari")
 
-echo "I am ${arr1[0]}"
-echo "I am ${arr1[1]}"
-echo "I am ${arr1[2]}"
+# size of above array is 3.
+# index are 0,1,2
+
+# list always starts with 0.
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[@]}"
+
+
+Movi=("Bharath" "Lokesh" "Honey")
+
+echo "I am ${Movi[0]}"
+echo "I am ${Movi[1]}"
+echo "I am ${Movi[2]}"
 
 
 #!/bin/bash
