1de3ab3a270d7d0946a623c7ac1876f52ee1fca7
“medurubharath” <medurubharathstvd@gmail.com>
2024-07-31 13:18:02 -0500

    updated 04 file

---
+++ 04-variables_read.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello Good Morning My dear Friend!"
+echo "Please enter your name::"
+
+read USERNAME
+
+echo "Please enter the password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME and the password is : $PASSWORD"
