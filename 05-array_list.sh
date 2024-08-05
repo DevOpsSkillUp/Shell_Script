06c0b39fad9f4ce7f0e50a43037400c1879daa1d
“medurubharath” <medurubharathstvd@gmail.com>
2024-08-04 23:34:18 -0500

    files

--- 05-array_list.sh
+++ 05-array_list.sh
@@ -2,11 +2,15 @@
 
 MOVIES=("RRR" "DjTillu" "murari")
 
-
 echo "First Movie is: ${MOVIES[0]}"
 echo "First Movie is: ${MOVIES[1]}"
 echo "First Movie is: ${MOVIES[2]}"
 
-arra=("Hello" "Good Morning" "Bye!")
+arra=("Hello" "Good" "Thankyou, Bye!")
 echo "${arra[0]} sir, i am ${arra[1]}"
 echo "${arra[2]}"
+
+abc=("a" "b" "c")
+echo "A: ${abc[0]}"
+echo "B: ${abc[1]}"
+echo "C: ${abc[2]}"
